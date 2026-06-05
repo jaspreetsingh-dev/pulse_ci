b177640c4bc34612457af172859fa51d56c9ec1f
jaspreetsingh-dev <jaspreetsinghh.dev@gmail.com>
2026-06-05 12:16:54 +0530

    feat: add check_secrets to checker.py

--- backend/checker.py
+++ backend/checker.py
@@ -13,3 +13,24 @@ def check_dependencies(repo_path):
             "passed": False,
             "reason": "recommended: please add requirements.txt or package.json"
         }
+
+def check_secrets(repo_path):
+    keywords = ["password=", "api_key=", "secret=", "db_password="]
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            try:
+                with open(full_path, "r") as f:
+                    for line in f:
+                        line = line.lower()
+                        for keyword in keywords:
+                            if keyword in line:
+                                return {"passed": False,
+                                       "reason": "confidentials are hardcoded"
+                                       }
+            except:
+                continue
+        return {
+            "passed": True,
+            "reason": "No confidentials are hardcoded"
+        }
