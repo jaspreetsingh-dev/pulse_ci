d8f5d198b9b735e4ab8509f42bdf34642df77bc9
jaspreetsingh-dev <jaspreetsinghh.dev@gmail.com>
2026-09-25 22:28:40 +0530

    Fix container dependencies and deployment configuration

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -48,7 +48,7 @@ docker run -d \
   --log-opt awslogs-region="${region}" \
   --log-opt awslogs-group="/pulse-ci/application" \
   --log-opt awslogs-create-group="false" \
-  --log-opt awslogs-stream="ec2-${HOSTNAME}" \
+  --log-opt awslogs-stream="ec2-$${HOSTNAME}" \
   -e DB_HOST="$DB_HOST" \
   -e DB_NAME="$DB_NAME" \
   -e DB_USER="$DB_USER" \
